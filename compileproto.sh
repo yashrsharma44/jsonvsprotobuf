a01dc17c5df23fd7f6ebcc17feceeaa3a8aeee4f
Yash Sharma <yashrsharma44@gmail.com>
2021-01-29 10:52:01 +0530

    Added scripts for demonstrating different benchmarks
    Signed-off-by: Yash Sharma <yashrsharma44@gmail.com>

--- compileproto.sh
+++
@@ -1,2 +0,0 @@
-echo "Compiling Protobuf....\n"
-protoc --go_out=. --go-grpc_opt=require_unimplemented_servers=false --go-grpc_out=. test_pb.proto
---
+++ scripts/cpu-profiling.sh
@@ -0,0 +1,1 @@
+go test -bench=. -benchmem -cpuprofile=jsonvsproto.cpu
